1c95d7bc328cffea67894b9b90d55d189c5ffe6a
sravani <sandhyapyla.8@gmail.com>
2024-07-25 12:35:19 +0530

    working css on product list

--- frontend/src/components/ProductList.js
+++ frontend/src/components/ProductList.js
@@ -1,16 +1,18 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchProducts } from '../features/products/productSlice';
-import { Link } from 'react-router-dom';
-import Navbar from './Navbar';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchProducts } from "../features/products/productSlice";
+import { Link } from "react-router-dom";
+import Navbar from "./Navbar";
+import "./ProductList.css"
+
 
 const ProductList = () => {
   const dispatch = useDispatch();
   const products = useSelector((state) => state.products.products);
   const status = useSelector((state) => state.products.status);
-const localstoragetoken=localStorage.getItem('token')
+  const localstoragetoken = localStorage.getItem("token");
   useEffect(() => {
-    if (status === 'idle') {
+    if (status === "idle") {
       dispatch(fetchProducts());
     }
   }, [status, dispatch]);
@@ -18,19 +20,37 @@ const localstoragetoken=localStorage.getItem('token')
   return (
     <>
       <Navbar />
-      <div className='product-list'>
-      {/* <h1>Product List</h1> */}
-      {localstoragetoken&&<ul>
-          {products.map((product) => (
-            <li key={product.id}>
-              <h2>{product.name}</h2>
-              <p>{product.price}</p>
-              <p>{product.description}</p>
-              <Link to={`/products/${product._id}`}>View Details</Link>
-            </li>
-          ))}
-        </ul>}
-      {/* {status === 'loading' ? (
+      <div className="product-list-container">
+        {/* <h1>Product List</h1> */}
+        {localstoragetoken && (
+          <div className="product-container">
+            <div className="product-list-container">
+              <ul className="product-list">
+                {products.map((product) => (
+                  <li key={product.id} className="product-item">
+                    <img
+                  className="product-image"
+                  src="./image.jpg"
+                  alt="dummy-image"
+                />
+                   <div className="product-details">
+                  <h2 className="product-name">{product.name}</h2>
+                </div>
+                <p className="product-price">Price: ${product.price}</p>
+                <p className="product-description">{product.description}</p>
+                    <div className="product-links">
+                  <Link to={`/products/${product._id}`} className="view-details">
+                    View Details
+                  </Link>
+                </div>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        )}
+
+        {/* {status === 'loading' ? (
         <p>Loading...</p>
       ) : (
         <ul>
